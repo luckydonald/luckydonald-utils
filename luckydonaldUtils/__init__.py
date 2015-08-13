9a1b85dfcfb9aa5ecfc63dff7e6a119108261645
luckydonald <luckydonald@flutterb.at>
2015-08-13 19:34:41 +0200

    fixed not possible to install due to import fail v0.4

--- luckydonaldUtils/__init__.py
+++ luckydonaldUtils/__init__.py
@@ -2,8 +2,5 @@
 __author__ = 'luckydonald'
 
 # DO NOT REMOVE the following, to allow 'from luckydonaldUtils import logging'
-from .Logging import logging as Logging
-
-
-VERSION = "0.3"
+VERSION = "0.4"
 
