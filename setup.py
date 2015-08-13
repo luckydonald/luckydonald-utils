cc29af0eaa1232181780ebc3bc1b6c35de86f288
luckydonald <luckydonald@flutterb.at>
2015-08-13 18:09:21 +0200

    Fixed setup.py to call it "luckydonald-utils" on PyPi

--- setup.py
+++ setup.py
@@ -5,8 +5,13 @@ from setuptools import setup
 
 long_description = """A collection of utilities I use across different projects"""
 
+# http://peterdowns.com/posts/first-time-with-pypi.html
+# $ python setup.py register -r pypi
+# $ python setup.py sdist upload -r pypi
+
 setup(
-	name="luckydonald's utilities",
+	name="luckydonald-utils",
+	packages=['luckydonaldUtils'],
 	version="0.1",
 	author="luckydonald",
 	author_email="code@luckydonald.de",
@@ -14,7 +19,6 @@ setup(
 	license="BSD",
 	keywords="example documentation tutorial",
 	url="https://github.com/luckydonald/python-utils",
-	packages=['luckydonaldUtils'],
 	install_requires=["pip", "importlib"],
 	long_description=long_description,
 	classifiers=[
