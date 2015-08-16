c8b4137b31d8b24cf195f470501135304f5988ea
luckydonald <luckydonald@flutterb.at>
2015-08-17 01:39:39 +0200

    Fixed python 3 compatibility mode working again.

--- luckydonaldUtils/__init__.py
+++ luckydonaldUtils/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 __author__ = 'luckydonald'
 
-# DO NOT REMOVE the following, to allow 'from luckydonaldUtils import logging'
-VERSION = "0.012"
+VERSION = "0.013"
 
--- luckydonaldUtils/time/__init__.py
+++ luckydonaldUtils/time/__init__.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ = 'luckydonald'
 
-from luckydonaldUtils.logger import logging  # pip install luckydonald-utils
-logger = logging.getLogger(__name__)
-
-
-# -*- coding: utf-8 -*-
-__author__ = 'luckydonald'
-
-from luckydonaldUtils.logger import logging
+import sys
 from ..dependencies import import_or_install
-
+from ..logger import logging
 logger = logging.getLogger(__name__)
-
 import datetime as datetime
 import_or_install("pytz", "pytz")
 from pytz import timezone
@@ -20,13 +12,23 @@ from pytz import timezone
 __all__ = ["get_local_time", "loop_seconds_between_times"]
 
 
-def get_local_time(time, time_zone):
+if sys.version < "3":
+	def get_local_time(time, time_zone):
+		return _get_local_time(time, time_zone)
+	def loop_seconds_between_times(datetime1, datetime2):
+		return _loop_seconds_between_times(datetime1, datetime2)
+else:
+	from .py3 import get_local_time, loop_seconds_between_times
+#end if
+
+
+def _get_local_time(time, time_zone):
 	if time_zone is None:
 		time_zone = "Europe/Berlin" # todo: config
 	return timezone('UTC').localize(time).astimezone(timezone(time_zone))
 
 
-def loop_seconds_between_times(datetime1, datetime2):
+def _loop_seconds_between_times(datetime1, datetime2):
 	delta = datetime2 - datetime1
 	seconds = delta.total_seconds()
 	for x in range(0, int(seconds)):
