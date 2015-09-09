2a56867f586685e37bdb137be505a622427c1c55
luckydonald <luckydonald@flutterb.at>
2015-09-09 22:24:37 +0200

    Added `py2` and `py3` boolean to root, indicating python 2.x or 3.x. ```from luckydonaldUtils import py2```  v0.016

--- luckydonaldUtils/__init__.py
+++ luckydonaldUtils/__init__.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
 __author__ = 'luckydonald'
 
-VERSION = "0.015"
+VERSION = "0.016"
+
+from sys import version as python_version
+
+py2 = True if python_version < '3' else False
+py3 = not py2
