4f3cc114583481cc088528f7a04f2f6eaab172e6
luckydonald <luckydonald@flutterb.at>
2015-09-10 17:43:32 +0200

    Added json helpers for https://github.com/luckydonald/shairport-decoder

---
+++ luckydonaldUtils/json.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from . import py2
+
+__author__ = 'luckydonald'
+
+
+def to_jsonable_dict(skip_keys=list()):
+	dict(to_jsonable_dict(skip_keys=skip_keys))
+
+if py2:
+	def to_jsonable_dict_iterator(dict, skip_keys=list()):
+		for key, value in dict.iteritems():
+			if key in skip_keys:
+				continue
+			if isinstance(value, datetime):
+				yield (key, str(value.isoformat()))
+			else:
+				yield (key, value)
+else:
+	def to_jsonable_dict_iterator(dict, skip_keys=list()):
+		for key, value in dict.items():
+			if key in skip_keys:
+				continue
+			if isinstance(value, datetime):
+				yield (key, str(value.isoformat()))
+			else:
+				yield (key, value)
