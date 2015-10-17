6494180600f449b2af0ad1ddd7ab8ff5be5f2924
luckydonald <luckydonald@flutterb.at>
2015-10-17 14:44:19 +0200

    Now with Django plugin, an manual CSRF check. Version Bump 0.22

---
+++ luckydonaldUtils/django/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+__author__ = 'luckydonald'
+
+from ..dependencies import import_or_install
+import_or_install("django", "django")
+
---
+++ luckydonaldUtils/django/csrf.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+__author__ = 'luckydonald'
+
+
+from django.middleware.csrf import CsrfViewMiddleware
+
+def check_csrf(request):
+	reason = CsrfViewMiddleware().process_view(request, None, (), {})
+	if reason:
+		# CSRF failed
+		return False
+	return True
