17d5c805e6508bc2485b9c9b720f548da7120b8f
luckydonald <luckydonald@flutterb.at>
2015-10-17 16:10:24 +0200

    Real Version Bump 0.24

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
 	author_email="code@luckydonald.de",
 	description=long_description,
 	license="GPLv3+",
-	keywords="luckydonald utils utilities utility python dependencies dependency download progress bar encoding files interactions json update store text xml time network logger color images webserver django CSRF",
+	keywords="luckydonald utils utilities utility python dependencies dependency download progress bar encoding files interactions json update store text xml time network logger color images webserver django CSRF headers eastereggs",
 	url="https://github.com/luckydonald/python-utils",
 	install_requires=install_requires,
 	long_description=long_description,
