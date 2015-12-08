0a8ac67a7677651368e3e2e8fbede7a4c1d07a80
luckydonald <luckydonald@flutterb.at>
2015-12-08 23:13:13 +0100

    Version Bump v0.34

--- luckydonaldUtils/__init__.py
+++ luckydonaldUtils/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'luckydonald'
 
-VERSION = "0.33"  # had a leading zero before version "0.20" (i.e. "0.019").  PyPI always removed that anyway.
+VERSION = "0.34"  # had a leading zero before version "0.20" (i.e. "0.019").  PyPI always removed that anyway.
 
 
 from sys import version as python_version
