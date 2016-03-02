83ed619028d8921cfa78f2eb5a321cf20847fc04
luckydonald <luckydonald@flutterb.at>
2016-03-02 21:39:36 +0100

    PEP 8.

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup
 from luckydonaldUtils import VERSION
 from luckydonaldUtils.dependencies import find_submodules
 
-
 long_description = """A collection of utilities I use across different projects"""
 
 # http://peterdowns.com/posts/first-time-with-pypi.html
@@ -16,26 +15,26 @@ main_package = 'luckydonaldUtils'
 
 install_requires = ["pip", "setuptools"]  # both should be installed by default.
 try:
-	import importlib
+    import importlib
 except ImportError:
-	install_requires.append('importlib')
+    install_requires.append('importlib')
 
 setup(
-	name="luckydonald-utils",
-	packages=find_submodules(main_package),
-	version=VERSION,
-	author="luckydonald",
-	author_email="code@luckydonald.de",
-	description=long_description,
-	license="GPLv3+",
-	keywords="luckydonald utils utilities utility python dependencies dependency download progress bar encoding files interactions json update store text xml time network logger color images webserver django CSRF headers eastereggs",
-	url="https://github.com/luckydonald/python-utils",
-	install_requires=install_requires,
-	long_description=long_description,
-	classifiers=[
-		"Development Status :: 4 - Beta",
-		"Intended Audience :: Developers",
-		"Topic :: Utilities",
-		"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-	],
+    name="luckydonald-utils",
+    packages=find_submodules(main_package),
+    version=VERSION,
+    author="luckydonald",
+    author_email="code@luckydonald.de",
+    description=long_description,
+    license="GPLv3+",
+    keywords="luckydonald utils utilities utility python dependencies dependency download progress bar encoding files interactions json update store text xml time network logger color images webserver django CSRF headers eastereggs",
+    url="https://github.com/luckydonald/python-utils",
+    install_requires=install_requires,
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ],
 )
