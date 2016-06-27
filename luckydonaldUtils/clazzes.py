c422a07fd78f319fab31419d77e1f94437a97e8d
luckydonald <luckydonald@flutterb.at>
2016-06-27 12:57:26 +0200

    Fixed Singleton class, there where some errors if you provided no `args` or no `kwargs`.

--- luckydonaldUtils/clazzes.py
+++ luckydonaldUtils/clazzes.py
@@ -2,6 +2,9 @@
     Only one Instance of the class.
     Like in Java .getInstance()
 
+
+    Make a class Singleton:
+
     Python 3:
     apply with keyword argument in the base-class list:
     >>> class Example(object, metaclass = Singleton):
@@ -12,6 +15,18 @@
     >>> class Exampe(object):
     >>>    __metaclass__ = Singleton
     >>>    pass
+
+
+    Usage of the class:
+
+    At the first call you have to supply the parameters for class, as usual.
+    Afterwards you can omit them, they will be ignored if given.
+
+    Create the instance:
+    >>> whatever = Example("arguments", "go", "here", as_well_as="keyword args")
+
+    Use a already created instance:
+    >>> something = Example()  # note: No args or kwargs required here.
 """
 
 
@@ -20,5 +35,8 @@ class Singleton(type):
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
+            args = args if args else []
+            kwargs = kwargs if kwargs else {}
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+        # end def
