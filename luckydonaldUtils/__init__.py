4d0beac2e80073c9b5bd7915a8136b7e737c3266
luckydonald <luckydonald@flutterb.at>
2016-08-15 05:38:37 +0200

    Bump version: 0.47 → 0.48

--- luckydonaldUtils/__init__.py
+++ luckydonaldUtils/__init__.py
@@ -2,7 +2,7 @@
 from sys import version as python_version
 
 __author__ = 'luckydonald'
-__version__ = "0.47"  # had a leading zero before version "0.20" (i.e. "0.019").  PyPI always removed that anyway.
+__version__ = "0.48"  # had a leading zero before version "0.20" (i.e. "0.019").  PyPI always removed that anyway.
 VERSION = __version__
 
 py2 = True if python_version < '3' else False
