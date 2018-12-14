6367e45fec06e6146653486d70419ab87b877004
luckydonald <luckydonald@flutterb.at>
2018-12-14 21:14:05 +0100

    Now has l_get instead of a l inside of create_l.

--- luckydonaldUtils/tg_bots/language.py
+++ luckydonaldUtils/tg_bots/language.py
@@ -8,9 +8,11 @@ try:
 except ImportError:  # pragma nocover
     Update = import_or_install("pytgbot.api_types.receivable.updates", "Update")  # pip install pytgbot
     Message = import_or_install("pytgbot.api_types.receivable.updates", "Message")  # pip install pytgbot
-
-
 # end try
+
+__all__ = ['get_language_code', 'l_from']
+
+LangClass = TypeVar('LangClass')
 
 
 def get_language_code(update_msg_or_language_code: Union[Update, Message, str, None] = None) -> Optional[str]:
@@ -30,55 +32,42 @@ def get_language_code(update_msg_or_language_code: Union[Update, Message, str, N
         # end if
     # end if
     return update_msg_or_language_code
-
-
 # end def
 
 
-T = TypeVar('T')
-
-
-def create_l(DefaultLang: Type[T], LANG: Dict[str, T]) -> Callable:
+def l_get(
+        language_dict: Dict[str, LangClass], update_msg_or_language_code: Union[Update, Message, str, None] = None
+) -> LangClass:
     """
-    Creates a new l function.
-    :param DefaultLang:
-    :param LANG:
-    :return:
+    Retrieves a translation string for a given update/message/language_code.
+
+    :param language_dict: the array containing the languages.
+    :param update_msg_or_language_code: The stuff containing language info.
+    :return: the fitting language class.
     """
-    def l(update_msg_or_language_code: Union[Update, Message, str, None] = None) -> DefaultLang:
-        """
-        Retrieves a translation string for a given update/message/language_code.
+    lang = get_language_code(update_msg_or_language_code)
 
-        :param update_msg_or_language_code: The stuff containing language info.
-        :return: the fitting language class.
-        """
-        lang = get_language_code(update_msg_or_language_code)
+    # if it is None (default, or getting from message failed), use the default
+    if lang is None:
+        return language_dict['default']
+    # end if
 
-        # if it now is None (default, or getting from message failed), use the default
-        if lang is None:
-            return LANG['default']
-        # end if
+    if lang in language_dict:
+        return language_dict[lang]
+    # end if
 
-        if lang in LANG:
-            return cast(DefaultLang, LANG[lang])
-        # end if
+    # try splitting it "de_DE" => "de"
+    part = lang.split('-')[0]
+    if part in language_dict:
+        return language_dict[part]
+    # end of
 
-        # try splitting it "de_DE" => "de",
+    # try splitting it "de-DE" => "de"
+    part = lang.split('_')[0]
+    if part in language_dict:
+        return language_dict[part]
+    # end if
 
-        part = lang.split('-')[0]
-        if part in LANG:
-            return cast(DefaultLang, LANG[part])
-        # end def
-
-        # try splitting it "de-DE" => "de",
-        part = lang.split('_')[0]
-        if part in LANG:
-            return cast(DefaultLang, LANG[part])
-        # end def
-        # or use the default
-
-        return cast(DefaultLang, LANG['default'])
-    # end def
-
-    return l
+    # nothing did match, use the default
+    return language_dict['default']
 # end def
