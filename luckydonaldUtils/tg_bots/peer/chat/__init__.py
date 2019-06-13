e30bc52654c56849e8bf9b2c277ffb660956bf14
luckydonald <mac2019.git@luckydonald.de>
2019-06-13 20:11:44 +0200

    [tg_bots] added missing `__init__.py`s.

---
+++ luckydonaldUtils/tg_bots/peer/chat/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from luckydonaldUtils.logger import logging
+
+__author__ = 'luckydonald'
+
+logger = logging.getLogger(__name__)
+if __name__ == '__main__':
+    logging.add_colored_handler(level=logging.DEBUG)
+# end if
