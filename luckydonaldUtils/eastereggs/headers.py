84cb5d3bbba9f82979a82db1c261511b9a7b7ecb
luckydonald <mac2019.git@luckydonald.de>
2019-07-14 15:21:43 +0200

    [eastereggs] Added ponies module.

--- luckydonaldUtils/eastereggs/headers.py
+++ luckydonaldUtils/eastereggs/headers.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-from random import randint
+from random import choice
 from .quotes import get_quote
+from .ponies import WAIFU
 
 __author__ = 'luckydonald'
 
 
 def get_headers(language="en"):
     headers = {}
-    headers["Server"] = ["iPod Touch, iOS 2.3.2", "Banana 0.2", "KONICHIWA/1.0", "'; DROP TABLE servertypes; -"][randint(0,3)]
-    headers["X-Powered-By"] = ["Magical Ponies", "Rats in our Basement", "Unicorns", "Friendship", "TONS OF SUGAR", "coffee", "Bananas and Rum"][randint(0,6)]
-    headers["X-Best-Pony"] = "Littlepip"
+    headers["Server"] = choice(["iPod Touch, iOS 2.3.2", "Banana 0.2", "KONICHIWA/1.0", "'; DROP TABLE servertypes; -"])
+    headers["X-Powered-By"] = choice(["Magical Ponies", "Rats in our Basement", "Unicorns", "Friendship", "TONS OF SUGAR", "coffee", "Bananas and Rum"])
+    headers["X-Best-Pony"] = choice(WAIFU)
     headers["X-Answer"] = "42"
     headers["X-Never-Gonna"] = "Give you up."
     headers["X-Nananana"] = "Batman!"
---
+++ luckydonaldUtils/eastereggs/ponies.py
@@ -0,0 +1,37 @@
+from typing import Union, List, Tuple
+
+WAIFU = [
+    "Littlepip",
+    "Toaster Repair Pony",
+    "Littlepip",
+    "The Lightbringer",
+    "The Stable Dweller"
+]
+BEST_PONY = [
+    "Applejack",
+    "Daring Do",
+    "Littlepip",
+]
+
+
+""":var UNWORTHY: A list of either strings, or a fun-fact tuple."""
+UNWORTHY: List[Union[str, Tuple[str, str, str]]] = [
+    "Blackjack",
+    "Blackjerk",
+    "oc:blackjack",
+    "Security Mare (Blackjack)",
+    "Maiden of the Stars (Blackjack)",
+    "Queen Whiskey (Blackjack)",
+    "Queen Blackjack",
+    "Black-And-Red OC",
+    "Blackjack (Cyber-Unicorn with Cyber Wings)",
+    "Blackjack (Cyber Alicorn)",
+    "Blackjack (Unicorn Blank/Clone again after her original body was torn in two)",
+    "Blackjack (Biological Great-great-great-great-great-great-great-great-great Granddaughter of Twilight Sparkle)",
+    "Blackjack (Biological Great-great-great-great-great-great-great-great-great Granddaughter of Big Macintosh)",
+    "Blackjack Clone (Made out of Discords blood)",
+    ("Blackjack", "Did you know:", "Blackjack died up to 4 times:\n\nIntroduction (Alive): Chp. 1\nDead: Chp. 33\nAlive again: Chp. 34\nDead: Chp. 48\nAlive: Chp. 49\nPossibly Dead: Chp. 54\nAlive Chp.54 \nDead end of Chp.77 (Original body)\nAlive in Epilogue (Clone body, implied to be immune to age)"),
+    ("Blackjack", "Did you know:", "Blackjack has had two boats dropped on her in her time in Hoofington - three if you count an airship."),
+    ("Blackjack", "Did you know:", "In an interview with the fan website Equestria Daily, Somber was originally going to have Blackjack be killed off by the end of Chapter 33 while Rampage and Scotch Tape continue where she left off."),
+    ("Blackjack", "Did you know:", "Another running gag is Blackjack's ignorance that Littlepip is the famed Stable Dweller."),
+]
