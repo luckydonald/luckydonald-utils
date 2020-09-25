eabb84fd81a69fe17bf8b7a1e6f73bc129163caa
luckydonald <mac2019.git@luckydonald.de>
2020-09-25 16:34:19 +0200

    Breaking change: Drop TELEGRAM_DOMAIN_REGEX and USER_LINK_REGEX, those two and the underscore versions can be fount at regex.urls.telegram now. USER_LINK_REGEX

--- luckydonaldUtils/regex/telegram.py
+++ luckydonaldUtils/regex/telegram.py
@@ -16,6 +16,3 @@ USER_AT_REGEX = re.compile(_USER_AT_REGEX)
 from .urls.telegram import _TELEGRAM_DOMAIN_REGEX
 _FULL_USERNAME_REGEX = '(?P<prefix>(?P<domain>' + _TELEGRAM_DOMAIN_REGEX + ')|@)(?P<username>' + _USERNAME_REGEX + ')'
 FULL_USERNAME_REGEX = re.compile(_FULL_USERNAME_REGEX)
-
-# backwards compatibility:
-from .urls.telegram import TELEGRAM_DOMAIN_REGEX, _USER_LINK_REGEX, USER_LINK_REGEX
